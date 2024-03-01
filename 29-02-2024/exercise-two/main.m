ab86d612d34bc09215a0182568eefba0c330a624
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-03-01 16:49:51 -0300

    feat: find Hypotenuse value with `A` and `B` value entered by user

---
+++ 29-02-2024/exercise-two/main.m
@@ -0,0 +1,6 @@
+clear; clc; % You can call command in the same line
+a = input('Enter a value to A = ');
+b = input('Enter a value to B = ');
+c = sqrt(a^2 + b^2);
+disp('The Hypotenuse value is = ');
+disp(c);
