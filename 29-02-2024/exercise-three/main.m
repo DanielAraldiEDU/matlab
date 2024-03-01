d0c04516382d41888205470ac4fa8502b544a866
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-03-01 17:01:14 -0300

    temp: change name of `twoEleven` value to `negativeTwoEleven`

--- 29-02-2024/exercise-three/main.m
+++ 29-02-2024/exercise-three/main.m
@@ -1,9 +1,9 @@
 threeFour = 3.4;
 four = 4;
-twoEleven = -2.11;
+negativeTwoEleven = -2.11;
 
-A = 2 * ((threeFour + four ^ twoEleven) / 2.33 ^ four);
-B = ((5 ^ (7 + twoEleven) - 2) / (threeFour ^ (3.21 * four) + four));
+A = 2 * ((threeFour + four ^ negativeTwoEleven) / 2.33 ^ four);
+B = ((5 ^ (7 + negativeTwoEleven) - 2) / (threeFour ^ (3.21 * four) + four));
 
 E = A - B;
 disp(E);
