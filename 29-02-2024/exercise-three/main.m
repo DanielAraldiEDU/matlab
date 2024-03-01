4cd77674a2b6499c449bfc677ff53c21e57965e8
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-03-01 16:54:15 -0300

    feat: add arithmetic account resolved

---
+++ 29-02-2024/exercise-three/main.m
@@ -0,0 +1,9 @@
+threeFour = 3.4;
+four = 4;
+twoEleven = -2.11;
+
+A = 2 * ((threeFour + four ^ twoEleven) / 2.33 ^ four);
+B = ((5 ^ (7 + twoEleven) - 2) / (threeFour ^ (3.21 * four) + four));
+
+E = A - B;
+disp(E);
