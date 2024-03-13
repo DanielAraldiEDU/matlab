743fc1dc4128c29970533954f517e8f9f3051ff1
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-03-13 11:00:39 -0300

    feat: add sum values between 1 and 100

---
+++ 07-03-2024/exercise-one/main.m
@@ -0,0 +1,14 @@
+clc; clear;
+sum = 0;
+disp('******************')
+
+for i = 1:100
+    sum = sum + i;
+end
+
+disp(['Sum = ', num2str(sum)]);
+disp('******************')
+disp(' ');
+
+fprintf(' i = %d, Sum = %d \n', i, s);
+fprintf('Sum values between 1 and 100 = %d\n', s);
