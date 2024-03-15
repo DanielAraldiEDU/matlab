dadd48dfc9fdf0c98778e43ede3902c87fb0fa7b
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-03-14 21:15:35 -0300

    fix: add `repeats` value in the `while` condition

--- 07-03-2024/exercise-three/main.m
+++ 07-03-2024/exercise-three/main.m
@@ -5,7 +5,7 @@ repeats = input('Enter the repeats value to calculate the sum: ');
 counter = 1;
 accumulator = 0;
 
-while counter <= 100
+while counter <= repeats
     accumulator = accumulator + counter;
     counter = counter + 1;
 end
