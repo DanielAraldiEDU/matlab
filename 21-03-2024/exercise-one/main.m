8a4e7e7ba41f183e59c963ab9bf03232b132e512
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-03-21 21:08:19 -0300

    feat: create arrays and transversal arrays

---
+++ 21-03-2024/exercise-one/main.m
@@ -0,0 +1,12 @@
+a = [5, 3, -1];
+size(a); % 1 3 (1 row and 3 columns)
+a = a'; % transversal array
+ 
+b = [-3, 0, -1];
+size(b); % 1 3 (1 row and 3 columns)
+
+x = (-5:2);
+% x = -5 -4 -3 -2 -1 0 1 2
+y = (20:50); % It's the same thing above, but 20 and 50
+% z = -5 0 5 10 15 20
+z = (-5:5:20)
