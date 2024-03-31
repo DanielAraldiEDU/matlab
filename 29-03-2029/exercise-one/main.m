60deaba99b2a21faa948c76ebf84d34fc87f38c7
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-03-31 11:51:23 -0300

    fix: small adjusts in variables name

--- 29-03-2029/exercise-one/main.m
+++ 29-03-2029/exercise-one/main.m
@@ -1,8 +1,9 @@
-volume = 10; length = 5; root = 1.6; height = [0:0.1:5]';
+volume = 10; length = 5; root = 1.6; x = [0:0.1:5]';
 
-plot(x, fx(L, r, volume, height)); grid on;
+plot(x, fx(length, root, volume, x)); grid on;
 
 a = -1; b = 1.5;
-[c, counter] = bisection(a, b, error, L, r, V)
-h(V, r)
-diameter(r);
+[c, counter] = bisection(a, b, error, length, root, volume)
+h(volume, root)
+
+diameter(root);
