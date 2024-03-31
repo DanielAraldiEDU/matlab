96f8296ac6526f5e29535e598c7b0de3bae13b01
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-03-31 10:42:19 -0300

    fix: add `volume` paramenter to generate graph using `plot` function

--- 29-03-2029/exercise-one/main.m
+++ 29-03-2029/exercise-one/main.m
@@ -1,7 +1,3 @@
-volume = 10;
-length = 5;
-root = 1.6;
+volume = 10; length = 5; root = 1.6; height = [0:0.1:5]';
 
-height = [0:0.1:5]';
-plot(x, fx(L, r, height)); 
-grid on;
+plot(x, fx(L, r, volume, height)); grid on;
