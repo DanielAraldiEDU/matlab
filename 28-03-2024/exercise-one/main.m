c24c70efd6401b34ca18a39e360e331fa082a127
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-03-31 13:26:04 -0300

    fix: add `repeats` value as one hundred

--- 28-03-2024/exercise-one/main.m
+++ 28-03-2024/exercise-one/main.m
@@ -2,6 +2,7 @@ smallestLimit = 0;
 biggestLimit = 2;
 x0 = 1;
 delta = 0.0001;
+repeats = 100;
 
 [root, iterator] = bissecao(smallestLimit, biggestLimit, delta);
 otherRoot = newton(x0, repeats, delta)
