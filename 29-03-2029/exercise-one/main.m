079a4cdafa0bb8cc2f65d9fcc8a7b5bc4ca7a001
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-03-31 11:45:04 -0300

    feat: add new values and functions to `main` file

--- 29-03-2029/exercise-one/main.m
+++ 29-03-2029/exercise-one/main.m
@@ -1,3 +1,8 @@
 volume = 10; length = 5; root = 1.6; height = [0:0.1:5]';
 
 plot(x, fx(L, r, volume, height)); grid on;
+
+a = -1; b = 1.5;
+[c, counter] = bisection(a, b, error, L, r, V)
+h(V, r)
+diameter(r);
