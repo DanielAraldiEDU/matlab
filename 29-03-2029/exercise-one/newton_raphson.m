e78e8690d13afdb1cb303e4d25d453f2ebc5b20a
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-04-01 16:49:16 -0300

    temp: add `derivate` function in newton raphson

--- 29-03-2029/exercise-one/newton_raphson.m
+++ 29-03-2029/exercise-one/newton_raphson.m
@@ -1,10 +1,10 @@
-function [value] = newton_raphson(x0, repeats, tolerance, L, r, V)
+function [value] = newton_raphson(x0, repeats, tolerance, length, root, volume)
     x = x0;
     counter = 0;
-    maxval = 1000000.0;
+    max_value = 1000000.0;
 
     while (repeats > 0)
-	    xn = x - fx(L, r, V, x) / derivade(L, r, V, x);
+	    xn = x - fx(length, root, volume, x) / derivate(length, root, volume, x);
         error = abs((xn - x) / xn);
         counter = counter + 1;
 
@@ -14,7 +14,7 @@ function [value] = newton_raphson(x0, repeats, tolerance, L, r, V)
   		  return;
       end;
 
-	    if abs(fx(L, r, V, x)) > maxval
+	    if abs(fx(length, root, volume, x)) > max_value
 		    disp(counter);
 		    error('Divergent solution');
 		    break;
