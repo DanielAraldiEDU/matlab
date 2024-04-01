3c75d8a5059d0506bf780601607c787cd088f67f
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-04-01 13:31:01 -0300

    feat: add `title`, `xlabel` and `ylabel` fields to the graph

--- 29-03-2029/exercise-one/main.m
+++ 29-03-2029/exercise-one/main.m
@@ -1,6 +1,9 @@
 volume = 10; length = 5; root = 1.6; x = [0:0.1:5]';
 
 plot(x, fx(length, root, volume, x)); grid on;
+title('Alunos: Daniel Sansão Araldi e Rafael Mota Alves');
+xlabel('Eixo X');
+ylabel('Eixo Y');
 
 a = -1; b = 1.5;
 [c, counter] = bisection(a, b, error, length, root, volume)
