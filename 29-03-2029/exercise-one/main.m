80ab1953964761e107aabd47b6d3414a9fede856
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-04-02 11:30:40 -0300

    feat: add `newton_raphson` function to `main` file

--- 29-03-2029/exercise-one/main.m
+++ 29-03-2029/exercise-one/main.m
@@ -1,13 +1,20 @@
 volume = 10; length = 5; root = 1.6; x = [0:0.1:5]';
 
+h(volume, root)
+diameter(root)
+
 plot(x, fx(length, root, volume, x)); grid on;
 title('Alunos: Daniel Sansão Araldi e Rafael Mota Alves');
 xlabel('Eixo X');
 ylabel('Eixo Y');
 
-a = -1; b = 1.5;
+a = -1; b = 1.5; error = 0.00001; repeats = 100;
 [c, counter] = bisection(a, b, error, length, root, volume)
-h(volume, root)
 
-diameter(root);
 xx = x0(r);
+xn = newton_raphson(xx, 100, error, length, root, volume)
+
+plot(x, fx(length, root, volume, x)); grid on;
+title('Alunos: Daniel Sansão Araldi e Rafael Mota Alves');
+xlabel('Eixo X');
+ylabel('Eixo Y');
