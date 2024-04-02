b2b8561678315dbc757d6238b17908571f6431c9
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-04-02 13:37:42 -0300

    fix: small adjusts in variables name

--- 29-03-2029/exercise-one/functionX.m
+++ 29-03-2029/exercise-one/functionX.m
@@ -1,3 +1,3 @@
 function [value] = functionX(length, root, volume, height)
-    value = L * (((r ^ 2) .* acos((r - h) / r)) - ((r - h) .* sqrt((2 * r .* h) - (h .^ 2)))) - volume;
+    value = length * (((root ^ 2) .* acos((root - height) / root)) - ((root - height) .* sqrt((2 * root .* height) - (height .^ 2)))) - volume;
 end
