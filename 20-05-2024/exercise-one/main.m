a6e17327093a28f7b7058e229908418f0f069814
Daniel Sansão Araldi <100477637+DanielAraldiEDU@users.noreply.github.com>
2024-05-23 21:39:48 -0300

    feat: add more instructions

--- 20-05-2024/exercise-one/main.m
+++ 20-05-2024/exercise-one/main.m
@@ -6,6 +6,19 @@ numbersOne = ones(2, 1)
 
 Ab = [numbersOne x y]
 AbT = Triang(Ab)
-Xsol = Subst(AbT)
+Xsol1 = Subst(AbT)
 
-P1x0 = Xsol(1) + Xsol(2) * 0.2
+P1x0 = Xsol1(1) + Xsol1(2) * 0.2
+
+XYTable = [0.1 1.221; 0.6 3.320; 0.8 4.953]
+
+x = XYTable(:, 1)
+y = XYTable(:, 2)
+
+numbersOne = ones(2, 1)
+
+Ab = [numbersOne x x.^2 y]
+AbT = Triang(Ab)
+Xsol2 = Subst(AbT)
+
+P2x0 = Xsol2(1) + (Xsol2(2) * 0.2) + (Xsol2(3) * 0.2 ^ 2)
